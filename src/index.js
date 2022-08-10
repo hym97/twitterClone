30a0f9eedf60f455e78833026aa8153a1bd03012
hym97 <78656286+hym97@users.noreply.github.com>
2022-08-10 01:21:21 -0400

    Progress on Login Module

--- src/index.js
+++ src/index.js
@@ -12,22 +12,6 @@ import {firstPart,secondPart,footerInfo} from "./testData";
 import RightSideBar from "./main/SideBar/RightSideBar";
 import Signin from "./AuthModule/Signin/Signin";
 import SignInPwd from "./AuthModule/Signin/SignInPwd";
-// import SearchBar from "./main/SideBar/SearchBar/SearchBar";
-// import UserSearchBar from "./main/SideBar/Item/commons/UserSearchBar";
-// import SearchBarItem from "./main/SideBar/SearchBar/SearchBarItemPanel/SearchBarItem/SearchBarItem";
-// import SearchBarPanel from "./main/SideBar/SearchBar/SearchBarItemPanel/SearchBarPanel";
-
-// const user = {avatar:"", userName:"psyduckpsyduckpsyduckpsyducj", certified:true, time:"4 hours ago",promoted:true, link:"#"};
-// const trendings = {prefix:"HelloWorld", suffix:"this is ymh"};
-// const header = {user};
-// const header2 = {trendings};
-// const content = "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.";
-// const footer = {content:"2213131 tweets"}
-// const Items = [{header:header, content, footer:null, meta:1}, {header:header2, content, footer, meta:false}]
-// const psyduck = {avatar:`https://www.pngitem.com/pimgs/m/177-1772639_psyduck-pokemon-character-vector-art-pokemon-png-psyduck.png`,
-//     userName:{userId:"Psyduck#0054", userDisplayedName:"Psyduck"}, certified:true, time:"4 hours ago",promoted:true,
-//     link:"#", desc:"this is a psyduckkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkjdsiojfodshfushfufhdsuhfshhoahofsuahoudashiua"};
-//
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
