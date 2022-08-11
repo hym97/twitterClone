9b3e2e2d6848d752d4a95ae1a2e1358a8b6626bc
hym97 <78656286+hym97@users.noreply.github.com>
2022-08-11 01:49:04 -0400

    as

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,7 +6,7 @@ export const Main = () => {
     return (
         <>
             <h1>This is main page</h1>
-
+            <h2>Hello World</h2>
             <Link to={"/flow"}>Login</Link>
         </>
 
